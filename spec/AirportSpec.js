e9fc47c7f70cc02b0ec4a57d3f8e0308ee3a55fb
Colin Turner <colinturner34@gmail.com>
2017-04-11 12:27:34 +0100

    Add default capacity to airport

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -1,12 +1,19 @@
 describe("Airport", function(){
   var airport;
   beforeEach(function(){
-  airport = new Airport();
+    airport = new Airport();
+    console.log(airport);
   });
 
   describe('initialize', function(){
     it('checks default capacity is set', function(){
-      expect(airport.capacity).toEqual("DEFAULT_CAPACITY");
+      expect(airport.capacity()).toEqual(DEFAULT_CAPACITY);
+    });
+
+    it('checks default capacity can be overridden', function() {
+      airport = new Airport(80)
+      expect(airport.capacity()).toEqual(80)
     });
   });
+
 });
---
+++ src/Airport.js
@@ -0,0 +1,6 @@
+var Airport = function() {};
+var DEFAULT_CAPACITY = 5;
+
+Airport.prototype.capacity = function() {
+  return DEFAULT_CAPACITY;
+};
