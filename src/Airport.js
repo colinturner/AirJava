fd197da78d8537037e149e3a5ebe9df9add6fdc0
Colin Turner <colinturner34@gmail.com>
2017-04-12 11:35:43 +0100

    Add Weather class

--- src/Airport.js
+++ src/Airport.js
@@ -1,10 +1,17 @@
 'use strict';
 
 function Airport(){
+  this._weather = typeof weather !== 'undefined' ? weather : new Weather();
   this._hangar = [];
 }
-Airport.prototype.planes = function(){ return this._hangar; };
+Airport.prototype.planes = function(){
+  return this._hangar;
+ };
+
 Airport.prototype.clearForLanding = function(plane){
+  if(this.isStormy()) {
+    throw new Error('cannot land during storm');
+  }
   this._hangar.push(plane);
 };
 
@@ -13,8 +20,4 @@ Airport.prototype.clearForTakeOff = function(plane) {
     throw new Error('cannot takeoff during storm');
   }
   this._hangar = [];
-};
-
-Airport.prototype.isStormy = function() {
-  return false;
 };
