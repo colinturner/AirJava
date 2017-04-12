7603d63ecb3617e6aa796c46dfcb825aea11a4dd
Magnus Holm <mail@magnusholm.com>
2017-04-12 10:50:31 +0100

    Add #isStormy function

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -3,15 +3,37 @@
 describe('Airport', function(){
   var airport;
   var plane;
+
   beforeEach(function(){
     airport = new Airport();
     plane = jasmine.createSpy('plane', ['land']);
   });
+
   it('has no planes by default', function(){
     expect(airport.planes()).toEqual([]);
   });
+
   it('can clear planes for landing', function(){
     airport.clearForLanding(plane);
     expect(airport.planes()).toEqual([plane]);
   });
+
+  it('can clear planes for takeoff', function() {
+    airport.clearForLanding(plane);
+    airport.clearForTakeOff(plane);
+    expect(airport.planes()).toEqual([]);
+  });
+
+  it('can check for stormy weather conditions', function() {
+    expect(airport.isStormy()).toBeFalsy();
+  });
+
+describe('Under stormy conditions', function() {
+
+  it('does not clear planes for takeoff', function() {
+    spyOn(airport, 'isStormy').and.returnValue(true);
+    expect(function(){ airport.clearForTakeOff(plane); }).toThrowError('cannot takeoff during storm');
+  });
+});
+
 });
